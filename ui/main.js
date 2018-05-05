7428297c5e674204a7584869e68c287c8417c53a
naveenbiitk <37117375+naveenbiitk@users.noreply.github.com>
2018-05-05 12:15:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //to change the inner html
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+
+//to move the image
+var img=document.getElementById('madi');
+img.onclick=function()
+{
+    img.style.marginLeft='100px';
+};
